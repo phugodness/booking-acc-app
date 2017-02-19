5ccedaec1d52d4ec7c786c1cd0783ed3bc142c5a
Truong Van Phu <phugodness@gmail.com>
2017-02-19 08:43:48 +0700

    complete basic login with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
   get 'about/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'about#index'
+  root to: "home#index"
+
 end
