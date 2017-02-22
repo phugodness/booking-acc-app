fe65ae3f0656c525128eec5248cc032cbf37a392
Truong Van Phu <phugodness@gmail.com>
2017-02-23 06:03:55 +0700

    add paperclip and image for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  do_not_validate_attachment_file_type :image
+  has_attached_file :image, default_url: "/img/:style/missing.png"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   get 'home/index'
 
   get 'about/index'
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'about#index'
   root to: "home#index"
-
+  as :user do
+    get 'users/profile', :to => 'devise/registrations#edit', :as => :user_root
+  end
 end
