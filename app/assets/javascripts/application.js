9b97afe4632bbdc42897a3a7dc52754bdd7e05c3
Truong Van Phu <phugodness@gmail.com>
2017-02-23 04:32:34 +0700

    import bootstrap start

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min
+//= require agency.min
+//= require jquery.easing
+//= require contact_me
 //= require turbolinks
 //= require_tree .
