ced58e8ac6a1a5f463c438f8abe71b6500ee4bf4
Truong Van Phu <phugodness@gmail.com>
2017-04-23 19:11:38 +0700

    add room to modal in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @rooms = Room.limit(6)
+    @reservation = Reservation.new
+    # @hash = Gmaps4rails.build_markers(@room) do |room, marker|
+    #   marker.lat room.latitude
+    #   marker.lng room.longitude
+    # end
   end
 end
