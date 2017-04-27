f90318554c1f703099f3fa36937f7ffb7215ff5b
Truong Van Phu <phugodness@gmail.com>
2017-04-27 23:06:57 +0700

    add map for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,15 @@ class SearchController < ApplicationController
     @q = Room.ransack(params[:q])
     @searched_rooms = @q.result
     @reservation = Reservation.new
+    @hash = []
+    @searched_rooms.each_with_index do |room, i|
+      @hash[i] = Gmaps4rails.build_markers(room) do |r, marker|
+        marker.lat r.latitude
+        marker.lng r.longitude
+        marker.json({
+          custom_marker: "#{r.name}<br>#{r.price}$<br><img src='../img/home_marker.png' width='30' height='30'>"
+        })
+      end
+    end
   end
 end
