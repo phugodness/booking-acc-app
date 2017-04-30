b7789ea829c5388ecf5b1f961793e24b1dea9a08
Truong Van Phu <phugodness@gmail.com>
2017-05-01 02:40:44 +0700

    add css and refactor code in home/_room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,7 +13,7 @@ class RoomsController < ApplicationController
     @reservation = Reservation.new
     @room = Room.includes(:type_of_room, :user).find_by_id(params[:id])
     gon.booked_date = []
-    @room.room_reservations.map(&:reservation).collect do |x|
+    @room.room_reservations.includes(:reservation).map(&:reservation).collect do |x|
       x.checkin_date.upto(x.checkout_date) { |d| gon.booked_date << d.strftime('%d/%m/%Y') }
     end
 
