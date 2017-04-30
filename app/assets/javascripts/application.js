a13aba650b4fc18a76e9886b03f6dff060b65341
Truong Van Phu <phugodness@gmail.com>
2017-04-30 17:40:07 +0700

    add disable date to daterange

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,10 +31,73 @@ $(document).ready(function(){
   $('.datepicker').datepicker({
     format: 'mm-dd-yyyy'
   });
+
+  // Define the disabled date array
+  var disabledArr = ["12/04/2017","13/04/2017","12/05/2017"];
+
+  // Function to draw the calendar accounting the disabled dates.
   $('input[class="daterange"]').daterangepicker({
     locale: {
       format: 'DD/MM/YYYY'
+    },
+
+    isInvalidDate: function(arg){
+      //console.log(arg);
+
+      // Prepare the date comparision
+      var thisMonth = arg._d.getMonth()+1;   // Months are 0 based
+      if (thisMonth<10){
+        thisMonth = "0"+thisMonth; // Leading 0
+      }
+      var thisDate = arg._d.getDate();
+      if (thisDate<10){
+        thisDate = "0"+thisDate; // Leading 0
+      }
+      var thisYear = arg._d.getYear()+1900;   // Years are 1900 based
+
+      var thisCompare = thisDate +"/"+ thisMonth +"/"+ thisYear;
+      console.log(thisCompare);
+
+      if($.inArray(thisCompare,disabledArr)!=-1){
+        console.log("      ^--------- DATE FOUND HERE");
+        return true; //arg._pf = {userInvalidated: true};
+      }
     }
 
+  }).focus(); // This focus is to open the calendar on page load.
+
+
+  // Function to disallow a range selection that includes disabled dates.
+  $('input[class="daterange"]').on("apply.daterangepicker",function(e,picker){
+
+    // Get the selected bound dates.
+    var startDate = picker.startDate.format('DD/MM/YYYY')
+    var endDate = picker.endDate.format('DD/MM/YYYY')
+    console.log(startDate+" to "+endDate);
+
+    // Compare the dates again.
+    var clearInput = false;
+    for(i=0;i<disabledArr.length;i++){
+      if(startDate<disabledArr[i] && endDate>disabledArr[i]){
+        console.log("Found a disabled Date in selection!");
+        clearInput = true;
+      }
+    }
+
+    // If a disabled date is in between the bounds, clear the range.
+    if(clearInput){
+
+      // To clear selected range (on the calendar).
+      var today = new Date();
+      $(this).data('daterangepicker').setStartDate(today);
+      $(this).data('daterangepicker').setEndDate(today);
+
+      // To clear input field and keep calendar opened.
+      $(this).val("").focus();
+      console.log("Cleared the input field...");
+
+      // Alert user!
+      alert("Your range selection includes disabled dates!");
+    }
   });
 });
