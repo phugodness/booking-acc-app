2822d86ca7b42a46226dbe7b6ee0367847b49af3
Truong Van Phu <phugodness@gmail.com>
2017-05-06 00:37:12 +0700

    add css in show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,6 @@ module ApplicationHelper
   end
 
   def icon_content(icon, content, value)
-    content_tag :i, content, class: icon, style: "font-size:20px; text-decoration:#{ value ? ';' : 'line-through;' }", :'aria-hidden' => 'true'
+    content_tag :i, ' ' + content, class: icon, style: "font-size:20px; text-decoration:#{ value ? ';' : 'line-through;' }", :'aria-hidden' => 'true'
   end
 end
