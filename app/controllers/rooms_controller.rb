d31e6c687d45e24ecfe69f2c42fd050c36e380a5
Truong Van Phu <phugodness@gmail.com>
2017-05-08 23:54:40 +0700

    add CRUD amentity to room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -41,6 +41,7 @@ class RoomsController < ApplicationController
   # GET /rooms/new
   def new
     @room = Room.new
+    @room.build_amentity
   end
 
   # GET /rooms/1/edit
@@ -108,6 +109,6 @@ class RoomsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def room_params
-    params.require(:room).permit(Room::DEFAULT_PARAMS)
+    params.require(:room).permit(Room::DEFAULT_PARAMS << {amentity_attributes: Amentity:: DEFAULT_PARAMS})
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,16 @@
 class Room < ApplicationRecord
-  DEFAULT_PARAMS = [:type_of_room_id, :user_id, :name, :address, :number_of_guest, :price, :accomodates, :number_of_bed, :description, :house_rules, :longitude, :latitude, :number_of_room].freeze
+  DEFAULT_PARAMS = [:type_of_room_id, :user_id, :name, :address, :number_of_guest, :price, :accomodates, :number_of_bed, :description, :house_rules, :longitude, :latitude, :number_of_room]
 
   belongs_to :type_of_room
   belongs_to :user
-  has_one :amentity
+  has_one :amentity, dependent: :destroy
   has_many :image_rooms, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :reservations, dependent: :destroy
 
   validates :name, :address, :number_of_guest, :price, :accomodates, :number_of_bed, :description, :house_rules, presence: true
+
+  accepts_nested_attributes_for :amentity
 
   geocoded_by :address # can also be an IP address
   after_validation :geocode # auto-fetch coordinates
