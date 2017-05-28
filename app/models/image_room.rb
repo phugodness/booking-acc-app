3bea75b0be7bb12b51b3d2c5ef6f2d9ff0c5f84a
Truong Van Phu <phugodness@gmail.com>
2017-05-29 00:27:50 +0700

    fix some rubocop and view

--- app/models/image_room.rb
+++ app/models/image_room.rb
@@ -3,7 +3,7 @@ class ImageRoom < ApplicationRecord
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment	:image,
                        presence: true,
-                       content_type: { content_type: /\Aimage\/.*\Z/ },
+                       content_type: { content_type: %r{\Aimage\/.*\Z} },
                        size: { less_than: 1.megabyte }
   def url_medium
     image.url(:medium)
